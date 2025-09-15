8c664c60689b4dc9d1890ed6e313eb110dac33f6
Lauren981 <lauren.flores@unitec.edu>
2025-09-14 20:00:43 -0600

    s

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import BottomTabNavegacion from "./Components/BottomTabNavigation";
+import { StyleSheet } from 'react-native';
 import StackNavegacion from "./Components/StackNavigation";
-import { PerfilProvider } from "./context/PerfilContext";
+import GlobalProvider from "./Provider/GlobalProvider";  // 👈 Usamos el Provider correcto
 
 export default function App() {
   return (
-      <PerfilProvider>
-        <StackNavegacion />
-      </PerfilProvider>
+    <GlobalProvider>
+      <StackNavegacion />
+    </GlobalProvider>
   );
 }
 
--- Pages/Home.tsx
+++ Pages/Home.tsx
@@ -1,215 +1,201 @@
 import { Button, Text, TextInput, View, StyleSheet, Image, TouchableOpacity, Alert } from 'react-native';
-import {useNavigation} from "@react-navigation/core";
-import {useContext, useState} from "react";
-import GlobalContext from "../Provider/GlobalProvider";
+import { useNavigation } from "@react-navigation/core";
+import { useContext } from "react";
+import { GlobalContext } from "../Provider/GlobalProvider";  // 👈 Importa el context, no el provider
 import Logo from '../assets/imagenes/LogoCineplus.png';
 
-
 export default function Home() {
+  const { correoContext, setCorreoContext, passwordContext, setPasswordContext, idContext, setIdContext } = useContext(GlobalContext);
+  const navigation = useNavigation();
 
-    const {correoContext, setCorreoContext,passwordContext, setPasswordContext,idContext,setIdContext} = useContext(GlobalContext)
-    const navigation = useNavigation();
+  // 🔹 Login
+  const comprobarUsuario = async () => {
+    if (!correoContext || !passwordContext) {
+      Alert.alert("Error", "Debes ingresar correo y contraseña");
+      return;
+    }
 
+    try {
+      const response = await fetch("http://10.0.2.2:3000/usuarios/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          correo: correoContext,
+          password: passwordContext,
+        }),
+      });
 
-    //Login
-    const comprobarUsuario = async () => {
-        if (!correoContext || !passwordContext) {
-            Alert.alert("Error", "Debes ingresar correo y contraseña");
-            return;
+      const data = await response.json();
+
+      if (response.ok) {
+        const id = data.usuario?.id;
+        if (!id) {
+          Alert.alert("Error", "No se pudo obtener el ID del usuario");
+          return;
         }
+        setIdContext(id);
+        Alert.alert("Éxito", `Usuario encontrado, iniciando sesión`);
+        navigation.navigate("Perfil"); // 👈 Lleva a perfil
+      } else {
+        Alert.alert("Error", data.message || "Usuario no encontrado");
+      }
+    } catch (error) {
+      Alert.alert("Error", "No se pudo conectar con el servidor");
+      console.error(error);
+    }
+  };
 
-        try {
+  // 🔹 Crear usuario
+  const crearUsuario = async () => {
+    if (!correoContext || !passwordContext) {
+      Alert.alert("Error", "Debes ingresar correo y contraseña");
+      return;
+    }
 
-            const response = await fetch("http://10.0.2.2:3000/usuarios/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    correo: correoContext,
-                    password: passwordContext
-                }),
-            });
+    try {
+      const response = await fetch("http://10.0.2.2:3000/usuarios", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          correo: correoContext,
+          password: passwordContext,
+        }),
+      });
 
-            const data = await response.json();
+      const data = await response.json();
 
-            if (response.ok) {
-                const id = data.usuario?.id;
-                if (!id) {
-                    Alert.alert("Error", "No se pudo obtener el ID del usuario");
-                    return;
-                }
-                setIdContext(id);
-                Alert.alert("Éxito", `Usuario encontrado, iniciando sesión`);
-                navigation.navigate("Perfil");////MODIFICADO
-            } else {
-                Alert.alert("Error", data.message || "Usuario no encontrado");
-            }
-        } catch (error) {
-            Alert.alert("Error", "No se pudo conectar con el servidor");
-            console.error(error);
-        }
-    };
+      if (response.ok) {
+        Alert.alert("Éxito", "Usuario creado correctamente");
+        setIdContext(data.usuario.id);
+        navigation.navigate("Perfil");
+      } else {
+        Alert.alert("Error", data.message || "No se pudo crear el usuario");
+      }
+    } catch (error) {
+      Alert.alert("Error", "No se pudo conectar con el servidor");
+      console.error(error);
+    }
+  };
 
+  return (
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image source={Logo} style={styles.logo} />
+      </View>
 
-// 🔹 Función para crear usuario
-    const crearUsuario = async () => {
-        if (!correoContext || !passwordContext) {
-            Alert.alert("Error", "Debes ingresar correo y contraseña");
-            return;
-        }
+      <Text style={styles.heading}>INICIO DE SESIÓN</Text>
 
-        try {
-            const response = await fetch("http://10.0.2.2:3000/usuarios", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    correo: correoContext,
-                    password: passwordContext
-                }),
-            });
+      <View style={styles.formContainer}>
+        <Text style={styles.label}>CORREO</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Correo"
+          placeholderTextColor="#888"
+          value={correoContext}
+          onChangeText={text => setCorreoContext(text)}
+        />
 
-            const data = await response.json();
+        <Text style={styles.label}>PASSWORD</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          placeholderTextColor="#888"
+          secureTextEntry
+          value={passwordContext}
+          onChangeText={text => setPasswordContext(text)}
+        />
+      </View>
 
-            if (response.ok) {
-                Alert.alert("Éxito", "Usuario creado correctamente");
-                setIdContext(data.usuario.id);
-                navigation.navigate("Perfil");
-            } else {
-                Alert.alert("Error", data.message || "No se pudo crear el usuario");
-            }
-        } catch (error) {
-            Alert.alert("Error", "No se pudo conectar con el servidor");
-            console.error(error);
-        }
-    };
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={comprobarUsuario}>
+          <Text style={styles.buttonText}>Ingresar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.buttonSecondary]} onPress={crearUsuario}>
+          <Text style={styles.buttonText}>Crear usuario</Text>
+        </TouchableOpacity>
+      </View>
 
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.logoContainer}>
-                <Image
-                    source={Logo}
-                    style={styles.logo}
-                />
-            </View>
-
-            <Text style={styles.heading}>INICIO DE SESIÓN</Text>
-
-            <View style={styles.formContainer}>
-                <Text style={styles.label}>CORREO</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Correo"
-                    placeholderTextColor="#888"
-                    value={correoContext}
-                    onChangeText={text => setCorreoContext(text)}
-                />
-
-                <Text style={styles.label}>PASSWORD</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Password"
-                    placeholderTextColor="#888"
-                    secureTextEntry
-                    value={passwordContext}
-                    onChangeText={text => setPasswordContext(text)}
-                />
-            </View>
-
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity style={styles.button} onPress={comprobarUsuario}>
-                    <Text style={styles.buttonText}>Ingresar</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={[styles.button, styles.buttonSecondary]} onPress={crearUsuario}>
-                    <Text style={styles.buttonText}>Crear usuario</Text>
-                </TouchableOpacity>
-            </View>
-
-
-            <Button
-                title="Ver Películas populares"
-                onPress={() => navigation.navigate('Peliculas Populares')}
-            />
-        </View>
-    );
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FFFFFF',
-        alignItems: 'center',
-        paddingTop: 50,
-    },
-    logoContainer: {
-        alignItems: 'center',
-        marginBottom: 30,
-    },
-    logo: {
-        width: 130,
-        height: 130,
-        marginBottom: 5,
-    },
-    heading: {
-        fontSize: 28,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        marginBottom: 30,
-        color: '#000',
-    },
-    formContainer: {
-        width: '80%',
-    },
-    label: {
-        fontSize: 16,
-        color: '#0c0b0bff',
-        marginBottom: 5,
-        marginTop: 15,
-        fontWeight: 'bold'
-    },
-    input: {
-        backgroundColor: '#E0E0E0',
-        borderRadius: 8,
-        paddingHorizontal: 15,
-        paddingVertical: 12,
-        fontSize: 16,
-        borderWidth: 1,
-        borderColor: '#f7f2f2ff',
-    },
-    buttonContainer: {
-        marginTop: 30,
-        width: '80%',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    button: {
-        backgroundColor: '#000',
-        paddingVertical: 15,
-        borderRadius: 8,
-        alignItems: 'center',
-        width: '48%',
-    },
-    buttonSecondary: {
-        backgroundColor: '#555',
-    },
-    buttonText: {
-        color: '#FFF',
-        fontSize: 18,
-        fontWeight: 'bold'
-    },
-    registerButton: {
-        marginTop: 20,
-        padding: 10,
-    },
-    registerText: {
-        color: '#888',
-        fontSize: 16,
-    },
-    registerLink: {
-        color: '#000',
-        fontWeight: 'bold',
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    paddingTop: 50,
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  logo: {
+    width: 130,
+    height: 130,
+    marginBottom: 5,
+  },
+  heading: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 30,
+    color: '#000',
+  },
+  formContainer: {
+    width: '80%',
+  },
+  label: {
+    fontSize: 16,
+    color: '#0c0b0bff',
+    marginBottom: 5,
+    marginTop: 15,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#E0E0E0',
+    borderRadius: 8,
+    paddingHorizontal: 15,
+    paddingVertical: 12,
+    fontSize: 16,
+    borderWidth: 1,
+    borderColor: '#f7f2f2ff',
+  },
+  buttonContainer: {
+    marginTop: 30,
+    width: '80%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  button: {
+    backgroundColor: '#000',
+    paddingVertical: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    width: '48%',
+  },
+  buttonSecondary: {
+    backgroundColor: '#555',
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  registerButton: {
+    marginTop: 20,
+    padding: 10,
+  },
+  registerText: {
+    color: '#888',
+    fontSize: 16,
+  },
+  registerLink: {
+    color: '#000',
+    fontWeight: 'bold',
+  },
 });
